c6a210caa569aa051fbfb41b17b871dc2f04094a
Juan David Conde Martínez <davidconde1309@gmail.com>
2025-04-10 23:57:34 -0500

    style: :lipstick: Front agregado al sistema para testeo con la base de datos y su funcionamiento a gran escala (todo el sistema)
    By @juanconde025

---
+++ src/main/java/com/pruebatecnica/Sistema/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.Sistema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; 
+    }
+}
